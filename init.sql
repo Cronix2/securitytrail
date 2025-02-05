151b9cc540d683547092df357e979efacfe164c2
Cronix2 <clement.linossier@efrei.net>
2025-02-05 11:05:54 +0100

    added Docker configuration for MariaDB and phpMyAdmin with SQL init script

---
+++ init.sql
@@ -0,0 +1,11 @@
+USE subdomains;
+
+DROP DATABASE IF EXISTS test;
+DROP DATABASE IF EXISTS performance_schema;
+DROP DATABASE IF EXISTS mysql;
+
+CREATE TABLE IF NOT EXISTS `subdomains-link-to-IP` (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    subdomain VARCHAR(255) NOT NULL,
+    ip_address VARCHAR(45) NOT NULL
+);
